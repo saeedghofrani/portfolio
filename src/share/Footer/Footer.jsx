0ac611c7de377a8c189ed2092272f984b044872f
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2024-08-22 02:28:38 +0330

    fix footer

--- src/share/Footer/Footer.jsx
+++ src/share/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import "./Footer.css"; // Make sure to link the CSS file if it's not already linked
 
 function Footer() {
   return (
     <footer className="footer">
+      <hr className="footer-line" />
       <p>&copy; 2024 Saeed Ghofrani. All Rights Reserved.</p>
     </footer>
   );
