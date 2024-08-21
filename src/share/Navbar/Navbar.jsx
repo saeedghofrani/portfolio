ec186dde87c7b36b9db6f6c5b8be3ace458a9d7e
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2024-08-21 21:41:08 +0330

    menu

--- src/share/Navbar/Navbar.jsx
+++ src/share/Navbar/Navbar.jsx
@@ -1,18 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { FaHome, FaProjectDiagram, FaTools, FaEnvelope } from 'react-icons/fa';
+import { Link, useLocation } from 'react-router-dom';
 import style from './NavbarStyle.module.css';
 
-function Navbar() {
+const Navbar = () => {
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState(location.pathname);
+
+  const handleNavClick = (path) => {
+    setActiveLink(path);
+  };
+
   return (
     <nav className={style.navbar}>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/projects">Projects</Link></li>
-        <li><Link to="/skills">Skills</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li
+          className={activeLink === '/' ? style.active : ''}
+          onClick={() => handleNavClick('/')}
+        >
+          <Link to="/" className={style.navIcon}>
+            <FaHome />
+            <span className={style.tooltip}>Home</span>
+          </Link>
+        </li>
+        <li
+          className={activeLink === '/projects' ? style.active : ''}
+          onClick={() => handleNavClick('/projects')}
+        >
+          <Link to="/projects" className={style.navIcon}>
+            <FaProjectDiagram />
+            <span className={style.tooltip}>Projects</span>
+          </Link>
+        </li>
+        <li
+          className={activeLink === '/skills' ? style.active : ''}
+          onClick={() => handleNavClick('/skills')}
+        >
+          <Link to="/skills" className={style.navIcon}>
+            <FaTools />
+            <span className={style.tooltip}>Skills</span>
+          </Link>
+        </li>
+        <li
+          className={activeLink === '/contact' ? style.active : ''}
+          onClick={() => handleNavClick('/contact')}
+        >
+          <Link to="/contact" className={style.navIcon}>
+            <FaEnvelope />
+            <span className={style.tooltip}>Contact</span>
+          </Link>
+        </li>
       </ul>
     </nav>
   );
-}
+};
 
 export default Navbar;
