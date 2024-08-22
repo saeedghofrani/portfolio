eb97ce180adfcf716ccc2fecc2e225b400c2f3b0
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2024-08-22 04:44:43 +0330

    color

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,7 +1,4 @@
 import CV from '../../assets/cv.pdf';
-import githubIcon from '../../assets/github-dark.svg';
-import linkedinIcon from '../../assets/linkedin-dark.svg';
-import twitterIcon from '../../assets/twitter-dark.svg';
 import { useTheme } from '../../common/ThemeContext';
 import styles from './HeroStyles.module.css';
 import React from "react";
@@ -18,17 +15,6 @@ function Hero() {
         <p className={styles.description}>
           Hello! I'm Saeed Ghofrani, a passionate web developer with expertise in creating modern and efficient web applications using React. I specialize in delivering high-quality user experiences through clean and maintainable code. My goal is to leverage my skills to build innovative solutions that drive success for businesses and delight users.
         </p>
-        <div className={styles.socialLinks}>
-          <a className={styles.button} href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
-            <img  src={twitterIcon} alt="Twitter icon" />
-          </a>
-          <a className={styles.button} href="https://github.com/" target="_blank" rel="noopener noreferrer">
-            <img src={githubIcon} alt="Github icon" />
-          </a>
-          <a className={styles.button} href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">
-            <img src={linkedinIcon} alt="LinkedIn icon" />
-          </a>
-        </div>
       </div>
     </section>
   );
