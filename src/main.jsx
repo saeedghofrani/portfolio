3d59e9c1b0ea197acdc19be7ad0a599d435ef648
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2024-08-23 14:56:08 +0330

    background

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { ThemeProvider } from './common/ThemeContext.jsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ThemeProvider } from "./common/ThemeContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <App />      <div class="bg"></div>
+      <div class="bg bg2"></div>
+      <div class="bg bg3"></div>
     </ThemeProvider>
   </React.StrictMode>
 );
