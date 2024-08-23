7cb48652169e9024f1d445e254bf4961fd849053
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2024-08-23 22:27:25 +0330

    icons

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,42 +1,67 @@
-import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/fresh-burger.png';
-import hipsster from '../../assets/hipsster.png';
-import fitLift from '../../assets/fitlift.png';
-import ProjectCard from '../../common/ProjectCard';
+// src/components/Projects.js
+import React from "react";
+import styles from "./ProjectsStyles.module.css"; // Import the CSS module
 
-function Projects() {
+const Projects = () => {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard
-          src={viberr}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Viberr"
-          p="Streaming App"
-        />
-        <ProjectCard
-          src={freshBurger}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Fresh Burger"
-          p="Hamburger Restaurant"
-        />
-        <ProjectCard
-          src={hipsster}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Hipsster"
-          p="Glasses Shop"
-        />
-        <ProjectCard
-          src={fitLift}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="FitLift"
-          p="Fitness App"
-        />
+        <div className={styles.card}>
+          <div className={styles.cardImage}>
+            <img src="https://via.placeholder.com/100" alt="Card" />
+            <a className={styles.cardButton}>Click Me</a>
+          </div>
+          <div className={styles.cardContent}>
+            <h3 className={styles.cardTitle}>Card Title</h3>
+            <p className={styles.cardDescription}>
+              This is a brief description of the card. It gives a quick overview
+              of the content.
+            </p>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardImage}>
+            <img src="https://via.placeholder.com/100" alt="Card" />
+            <a className={styles.cardButton}>Click Me</a>
+          </div>
+          <div className={styles.cardContent}>
+            <h3 className={styles.cardTitle}>Card Title</h3>
+            <p className={styles.cardDescription}>
+              This is a brief description of the card. It gives a quick overview
+              of the content.
+            </p>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardImage}>
+            <img src="https://via.placeholder.com/100" alt="Card" />
+            <a className={styles.cardButton}>Click Me</a>
+          </div>
+          <div className={styles.cardContent}>
+            <h3 className={styles.cardTitle}>Card Title</h3>
+            <p className={styles.cardDescription}>
+              This is a brief description of the card. It gives a quick overview
+              of the content.
+            </p>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardImage}>
+            <img src="https://via.placeholder.com/100" alt="Card" />
+            <a className={styles.cardButton}>Click Me</a>
+          </div>
+          <div className={styles.cardContent}>
+            <h3 className={styles.cardTitle}>Card Title</h3>
+            <p className={styles.cardDescription}>
+              This is a brief description of the card. It gives a quick overview
+              of the content.
+            </p>
+          </div>
+        </div>
+        {/* Add more cards as needed */}
       </div>
     </section>
   );
-}
+};
 
 export default Projects;
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,53 +1,121 @@
-import React from 'react';
-import styles from './SkillsStyles.module.css';
+import React from "react";
+import styles from "./SkillsStyles.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faDatabase,
+  faCode,
+  faChartLine,
+  faExpandArrowsAlt,
+  faClock,
+  faCubes,
+  faFileAlt,
+  faTasks,
+  faProjectDiagram,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Skills = () => {
   return (
     <section className={styles.skillsSection}>
-
+      <article className={styles.iconSection}>
+        <div class={styles.icnosSkill}>
+          <FontAwesomeIcon icon={faDatabase} />
+        </div>
+        <div class={styles.icnosSkill}>
+          <FontAwesomeIcon icon={faCode} />
+        </div>
+        <div class={styles.icnosSkill}>
+          <FontAwesomeIcon icon={faChartLine} />
+        </div>
+        <div class={styles.icnosSkill}>
+          <FontAwesomeIcon icon={faExpandArrowsAlt} />
+        </div>
+        <div class={styles.icnosSkill}>
+          <FontAwesomeIcon icon={faClock} />
+        </div>
+        <div class={styles.icnosSkill}>
+          <FontAwesomeIcon icon={faCubes} />
+        </div>
+        <div class={styles.icnosSkill}>
+          <FontAwesomeIcon icon={faFileAlt} />
+        </div>
+        <div class={styles.icnosSkill}>
+          <FontAwesomeIcon icon={faTasks} />
+        </div>
+        <div class={styles.icnosSkill}>
+          <FontAwesomeIcon icon={faProjectDiagram} />
+        </div>
+      </article>
+      <article>
       <div className={styles.skillCategory}>
         <h3>Full-Stack Proficiency</h3>
-        <p>Adept in Node.js (NestJS, TypeScript, Express), familiar with frontend frameworks (Angular), familiar with C# Language (ASP.NET Core).</p>
+        <p>
+          Adept in Node.js (NestJS, TypeScript, Express), familiar with frontend
+          frameworks (Angular), familiar with C# Language (ASP.NET Core).
+        </p>
       </div>
 
       <div className={styles.skillCategory}>
         <h3>Polyglot Persistence</h3>
-        <p>Extensive experience with a diverse range of databases (PostgreSQL - proficient, MongoDB - proficient, Redis - proficient, RabbitMQ, Neo4j, MySQL, SQL, SQLite).</p>
+        <p>
+          Extensive experience with a diverse range of databases (PostgreSQL -
+          proficient, MongoDB - proficient, Redis - proficient, RabbitMQ, Neo4j,
+          MySQL, SQL, SQLite).
+        </p>
       </div>
 
       <div className={styles.skillCategory}>
         <h3>Data Mastery</h3>
-        <p>Skilled in ORMs (Prisma, TypeORM, Sequelize) and crafting complex raw SQL and PostgreSQL queries.</p>
+        <p>
+          Skilled in ORMs (Prisma, TypeORM, Sequelize) and crafting complex raw
+          SQL and PostgreSQL queries.
+        </p>
       </div>
 
       <div className={styles.skillCategory}>
         <h3>Scalability Champion</h3>
-        <p>Proficient in deployment with Docker, Kubernetes, and Nginx for high-performance backend systems and container orchestration.</p>
+        <p>
+          Proficient in deployment with Docker, Kubernetes, and Nginx for
+          high-performance backend systems and container orchestration.
+        </p>
       </div>
 
       <div className={styles.skillCategory}>
         <h3>Real-Time Architect</h3>
-        <p>Implemented real-time functionalities using socket-based applications and Firebase for seamless data synchronization.</p>
+        <p>
+          Implemented real-time functionalities using socket-based applications
+          and Firebase for seamless data synchronization.
+        </p>
       </div>
 
       <div className={styles.skillCategory}>
         <h3>Microservices Mastermind</h3>
-        <p>Designed and developed microservices and RESTful APIs for complex projects.</p>
+        <p>
+          Designed and developed microservices and RESTful APIs for complex
+          projects.
+        </p>
       </div>
 
       <div className={styles.skillCategory}>
         <h3>Reporting Guru</h3>
-        <p>Developed comprehensive reporting systems for network projects and cryptocurrency exchanges.</p>
+        <p>
+          Developed comprehensive reporting systems for network projects and
+          cryptocurrency exchanges.
+        </p>
       </div>
 
       <div className={styles.skillCategory}>
         <h3>Project Management Pro</h3>
-        <p>Utilized Trello and GitHub effectively to manage tasks, milestones, and collaborate efficiently.</p>
+        <p>
+          Utilized Trello and GitHub effectively to manage tasks, milestones,
+          and collaborate efficiently.
+        </p>
       </div>
 
       <div className={styles.skillCategory}>
         <h3>CI/CD Automation Savvy</h3>
-        <p>Familiar with CI/CD pipelines for streamlined development workflows.</p>
+        <p>
+          Familiar with CI/CD pipelines for streamlined development workflows.
+        </p>
       </div>
 
       <div className={styles.skillCategory}>
@@ -57,13 +125,20 @@ const Skills = () => {
 
       <div className={styles.skillCategory}>
         <h3>Data-Driven Problem Solver</h3>
-        <p>Identifying critical issues, resulting in cost savings (30% reduction).</p>
+        <p>
+          Identifying critical issues, resulting in cost savings (30%
+          reduction).
+        </p>
       </div>
 
       <div className={styles.skillCategory}>
         <h3>Collaborative & Communication Skills</h3>
-        <p>Foster communication across teams for seamless user experiences. Excellent technical communication skills in English.</p>
+        <p>
+          Foster communication across teams for seamless user experiences.
+          Excellent technical communication skills in English.
+        </p>
       </div>
+      </article>
     </section>
   );
 };
