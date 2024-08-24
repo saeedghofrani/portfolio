7ab109e6b894381257ae50d8071ee30e4681e639
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2024-08-24 03:31:39 +0330

    skill icons

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styles from "./SkillsStyles.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -10,135 +10,146 @@ import {
   faCubes,
   faFileAlt,
   faTasks,
+  faTable,
   faProjectDiagram,
+  faCogs,
+  faChartBar,
+  faUsers,
+  faSearch,
+  faTachometerAlt
 } from "@fortawesome/free-solid-svg-icons";
 
 const Skills = () => {
+  const [hoveredSkill, setHoveredSkill] = useState(null);
+  const [selectedSkill, setSelectedSkill] = useState(null);
+  const iconSectionRef = useRef(null);
+
+  const handleMouseEnter = (skill) => {
+    if (!selectedSkill) {
+      setHoveredSkill(skill);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (!selectedSkill) {
+      setHoveredSkill(null);
+    }
+  };
+
+  const handleClick = (skill) => {
+    setSelectedSkill(skill === selectedSkill ? null : skill);
+  };
+
+  const handleClickOutside = (event) => {
+    if (iconSectionRef.current && !iconSectionRef.current.contains(event.target)) {
+      setSelectedSkill(null);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
+  const skills = [
+    {
+      icon: faCode,
+      name: "Full-Stack Proficiency",
+      description: "Adept in Node.js (NestJS, TypeScript, Express), familiar with frontend frameworks (Angular), familiar with C# Language (ASP.NET Core).",
+    },
+    {
+      icon: faDatabase,
+      name: "Polyglot Persistence",
+      description: "Extensive experience with a diverse range of databases (PostgreSQL - proficient, MongoDB - proficient, Redis - proficient, RabbitMQ, Neo4j, MySQL, SQL, SQLite).",
+    },
+    {
+      icon: faTable ,
+      name: "Data Mastery",
+      description: "Skilled in ORMs (Prisma, TypeORM, Sequelize) and crafting complex raw SQL and PostgreSQL queries.",
+    },
+    {
+      icon: faExpandArrowsAlt,
+      name: "Scalability Champion",
+      description: "Proficient in deployment with Docker, Kubernetes, and Nginx for high-performance backend systems and container orchestration.",
+    },
+    {
+      icon: faClock,
+      name: "Real-Time Architect",
+      description: "Implemented real-time functionalities using socket-based applications and Firebase for seamless data synchronization.",
+    },
+    {
+      icon: faCubes,
+      name: "Microservices Mastermind",
+      description: "Designed and developed microservices and RESTful APIs for complex projects.",
+    },
+    {
+      icon: faFileAlt,
+      name: "Reporting Guru",
+      description: "Developed comprehensive reporting systems for network projects and cryptocurrency exchanges.",
+    },
+    {
+      icon: faTasks,
+      name: "Project Management Pro",
+      description: "Utilized Trello and GitHub effectively to manage tasks, milestones, and collaborate efficiently.",
+    },
+    {
+      icon: faCogs ,
+      name: "CI/CD Automation Savvy",
+      description: "Familiar with CI/CD pipelines for streamlined development workflows.",
+    },
+    {
+      icon: faTachometerAlt,
+      name: "Thrive in Fast-Paced Environments",
+      description: "bility to manage complex projects while meeting deadlines.",
+    },
+    {
+      icon: faSearch ,
+      name: "Data-Driven Problem Solver",
+      description: "    Identifying critical issues, resulting in cost savings (30% reduction).",
+    },
+    {
+      icon: faUsers,
+      name: "Collaborative & Communication Skills",
+      description: "Foster communication across teams for seamless user experiences. Excellent technical communication skills in English.",
+    },
+  ];
   return (
     <section className={styles.skillsSection}>
-      <article className={styles.iconSection}>
-        <div class={styles.icnosSkill}>
-          <FontAwesomeIcon icon={faCode} />
-        </div>
-        <div class={styles.icnosSkill}>
-          <FontAwesomeIcon icon={faDatabase} />
-        </div>
-        <div class={styles.icnosSkill}>
-          <FontAwesomeIcon icon={faChartLine} />
-        </div>
-        <div class={styles.icnosSkill}>
-          <FontAwesomeIcon icon={faExpandArrowsAlt} />
-        </div>
-        <div class={styles.icnosSkill}>
-          <FontAwesomeIcon icon={faClock} />
-        </div>
-        <div class={styles.icnosSkill}>
-          <FontAwesomeIcon icon={faCubes} />
-        </div>
-        <div class={styles.icnosSkill}>
-          <FontAwesomeIcon icon={faFileAlt} />
-        </div>
-        <div class={styles.icnosSkill}>
-          <FontAwesomeIcon icon={faTasks} />
-        </div>
-        <div class={styles.icnosSkill}>
-          <FontAwesomeIcon icon={faProjectDiagram} />
-        </div>
+      <article className={styles.iconSection} ref={iconSectionRef}>
+        {skills.map((skill, index) => (
+          <div
+            key={index}
+            className={styles.icnosSkill}
+            onMouseEnter={() => handleMouseEnter(skill.name)}
+            onMouseLeave={handleMouseLeave}
+            onClick={() => handleClick(skill.name)}
+            style={{
+              opacity: selectedSkill && selectedSkill !== skill.name ? 0 : 1,
+              filter: hoveredSkill && hoveredSkill !== skill.name ? "brightness(0.5)" : "brightness(1)",
+              cursor: "pointer",
+            }}
+          >
+            <FontAwesomeIcon icon={skill.icon} />
+          </div>
+        ))}
       </article>
       <article>
-        <div className={styles.skillCategory}>
-          <h3>Full-Stack Proficiency</h3>
-          <p>
-            Adept in Node.js (NestJS, TypeScript, Express), familiar with
-            frontend frameworks (Angular), familiar with C# Language (ASP.NET
-            Core).
-          </p>
-        </div>
-
-        <div className={styles.skillCategory}>
-          <h3>Polyglot Persistence</h3>
-          <p>
-            Extensive experience with a diverse range of databases (PostgreSQL -
-            proficient, MongoDB - proficient, Redis - proficient, RabbitMQ,
-            Neo4j, MySQL, SQL, SQLite).
-          </p>
-        </div>
-
-        <div className={styles.skillCategory}>
-          <h3>Data Mastery</h3>
-          <p>
-            Skilled in ORMs (Prisma, TypeORM, Sequelize) and crafting complex
-            raw SQL and PostgreSQL queries.
-          </p>
-        </div>
-
-        <div className={styles.skillCategory}>
-          <h3>Scalability Champion</h3>
-          <p>
-            Proficient in deployment with Docker, Kubernetes, and Nginx for
-            high-performance backend systems and container orchestration.
-          </p>
-        </div>
-
-        <div className={styles.skillCategory}>
-          <h3>Real-Time Architect</h3>
-          <p>
-            Implemented real-time functionalities using socket-based
-            applications and Firebase for seamless data synchronization.
-          </p>
-        </div>
-
-        <div className={styles.skillCategory}>
-          <h3>Microservices Mastermind</h3>
-          <p>
-            Designed and developed microservices and RESTful APIs for complex
-            projects.
-          </p>
-        </div>
-
-        <div className={styles.skillCategory}>
-          <h3>Reporting Guru</h3>
-          <p>
-            Developed comprehensive reporting systems for network projects and
-            cryptocurrency exchanges.
-          </p>
-        </div>
-
-        <div className={styles.skillCategory}>
-          <h3>Project Management Pro</h3>
-          <p>
-            Utilized Trello and GitHub effectively to manage tasks, milestones,
-            and collaborate efficiently.
-          </p>
-        </div>
-
-        <div className={styles.skillCategory}>
-          <h3>CI/CD Automation Savvy</h3>
-          <p>
-            Familiar with CI/CD pipelines for streamlined development workflows.
-          </p>
-        </div>
-
-        <div className={styles.skillCategory}>
-          <h3>Thrive in Fast-Paced Environments</h3>
-          <p>Ability to manage complex projects while meeting deadlines.</p>
-        </div>
-
-        <div className={styles.skillCategory}>
-          <h3>Data-Driven Problem Solver</h3>
-          <p>
-            Identifying critical issues, resulting in cost savings (30%
-            reduction).
-          </p>
-        </div>
-
-        <div className={styles.skillCategory}>
-          <h3>Collaborative & Communication Skills</h3>
-          <p>
-            Foster communication across teams for seamless user experiences.
-            Excellent technical communication skills in English.
-          </p>
-        </div>
+        {skills.map((skill, index) => (
+          <div
+            key={index}
+            className={styles.skillCategory}
+            style={{
+              display: selectedSkill && selectedSkill !== skill.name ? "none" : "block",
+              opacity: hoveredSkill && hoveredSkill !== skill.name ? 0.3 : 1,
+              filter: hoveredSkill && hoveredSkill !== skill.name ? "brightness(0.5)" : "brightness(1)",
+            }}
+          >
+            <h3>{skill.name}</h3>
+            <p>{skill.description}</p>
+          </div>
+        ))}
       </article>
     </section>
   );
