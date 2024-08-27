d9a83cfbf41a742dd2ae5ba656f3407566e92f48
Saeed Ghofrani <sa.ghofraniivari@gmail.com>
2024-08-27 15:54:39 +0330

    skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -16,7 +16,7 @@ import {
   faChartBar,
   faUsers,
   faSearch,
-  faTachometerAlt
+  faTachometerAlt,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Skills = () => {
@@ -44,7 +44,10 @@ const Skills = () => {
   };
 
   const handleClickOutside = (event) => {
-    if (iconSectionRef.current && !iconSectionRef.current.contains(event.target)) {
+    if (
+      iconSectionRef.current &&
+      !iconSectionRef.current.contains(event.target)
+    ) {
       setSelectedSkill(null);
       setHoveredSkill(null); // Reset hover state when clicking outside
     }
@@ -61,99 +64,123 @@ const Skills = () => {
     {
       icon: faCode,
       name: "Full-Stack Proficiency",
-      description: "Adept in Node.js (NestJS, TypeScript, Express), familiar with frontend frameworks (Angular), familiar with C# Language (ASP.NET Core).",
+      description:
+        "Adept in Node.js (NestJS, TypeScript, Express), familiar with frontend frameworks (Angular), familiar with C# Language (ASP.NET Core).",
     },
     {
       icon: faDatabase,
       name: "Polyglot Persistence",
-      description: "Extensive experience with a diverse range of databases (PostgreSQL - proficient, MongoDB - proficient, Redis - proficient, RabbitMQ, Neo4j, MySQL, SQL, SQLite).",
+      description:
+        "Extensive experience with a diverse range of databases (PostgreSQL - proficient, MongoDB - proficient, Redis - proficient, RabbitMQ, Neo4j, MySQL, SQL, SQLite).",
     },
     {
       icon: faTable,
       name: "Data Mastery",
-      description: "Skilled in ORMs (Prisma, TypeORM, Sequelize) and crafting complex raw SQL and PostgreSQL queries.",
+      description:
+        "Skilled in ORMs (Prisma, TypeORM, Sequelize) and crafting complex raw SQL and PostgreSQL queries.",
     },
     {
       icon: faExpandArrowsAlt,
       name: "Scalability Champion",
-      description: "Proficient in deployment with Docker, Kubernetes, and Nginx for high-performance backend systems and container orchestration.",
+      description:
+        "Proficient in deployment with Docker, Kubernetes, and Nginx for high-performance backend systems and container orchestration.",
     },
     {
       icon: faClock,
       name: "Real-Time Architect",
-      description: "Implemented real-time functionalities using socket-based applications and Firebase for seamless data synchronization.",
+      description:
+        "Implemented real-time functionalities using socket-based applications and Firebase for seamless data synchronization.",
     },
     {
       icon: faCubes,
       name: "Microservices Mastermind",
-      description: "Designed and developed microservices and RESTful APIs for complex projects.",
+      description:
+        "Designed and developed microservices and RESTful APIs for complex projects.",
     },
     {
       icon: faFileAlt,
       name: "Reporting Guru",
-      description: "Developed comprehensive reporting systems for network projects and cryptocurrency exchanges.",
+      description:
+        "Developed comprehensive reporting systems for network projects and cryptocurrency exchanges.",
     },
     {
       icon: faTasks,
       name: "Project Management Pro",
-      description: "Utilized Trello and GitHub effectively to manage tasks, milestones, and collaborate efficiently.",
+      description:
+        "Utilized Trello and GitHub effectively to manage tasks, milestones, and collaborate efficiently.",
     },
     {
       icon: faCogs,
       name: "CI/CD Automation Savvy",
-      description: "Familiar with CI/CD pipelines for streamlined development workflows.",
+      description:
+        "Familiar with CI/CD pipelines for streamlined development workflows.",
     },
     {
       icon: faTachometerAlt,
       name: "Thrive in Fast-Paced Environments",
-      description: "Ability to manage complex projects while meeting deadlines.",
+      description:
+        "Ability to manage complex projects while meeting deadlines.",
     },
     {
       icon: faSearch,
       name: "Data-Driven Problem Solver",
-      description: "Identifying critical issues, resulting in cost savings (30% reduction).",
+      description:
+        "Identifying critical issues, resulting in cost savings (30% reduction).",
     },
     {
       icon: faUsers,
       name: "Collaborative & Communication Skills",
-      description: "Foster communication across teams for seamless user experiences. Excellent technical communication skills in English.",
+      description:
+        "Foster communication across teams for seamless user experiences. Excellent technical communication skills in English.",
     },
   ];
 
   return (
     <section className={styles.skillsSection}>
       <article className={styles.iconSection} ref={iconSectionRef}>
-        {skills.map((skill, index) => (
-          <div
-            key={index}
-            className={styles.icnosSkill}
-            onMouseEnter={() => handleMouseEnter(skill.name)}
-            onMouseLeave={handleMouseLeave}
-            onClick={() => handleClick(skill.name)}
-            style={{
-              // position: "fixed",
-              opacity: selectedSkill && selectedSkill !== skill.name ? 0 : 1,
-              filter: hoveredSkill && hoveredSkill !== skill.name ? "brightness(0.5)" : "brightness(1)",
-              cursor: "pointer",
-            }}
-          >
-            <FontAwesomeIcon icon={skill.icon} />
-          </div>
-        ))}
       </article>
       <article className={styles.skillText}>
         {skills.map((skill, index) => (
-          <div
-            key={index}
-            className={styles.skillCategory}
-            style={{
-              display: selectedSkill && selectedSkill !== skill.name ? "none" : "block",
-              opacity: hoveredSkill && hoveredSkill !== skill.name ? 0.3 : 1,
-              filter: hoveredSkill && hoveredSkill !== skill.name ? "brightness(0.5)" : "brightness(1)",
-            }}
-          >
-            <h3>{skill.name}</h3>
-            <p>{skill.description}</p>
+          <div className={styles.main}>
+            <div className={styles.iconSection} ref={iconSectionRef}>
+              <div
+                key={index}
+                className={styles.icnosSkill}
+                onMouseEnter={() => handleMouseEnter(skill.name)}
+                onMouseLeave={handleMouseLeave}
+                onClick={() => handleClick(skill.name)}
+                style={{
+                  // position: "fixed",
+                  opacity:
+                    selectedSkill && selectedSkill !== skill.name ? 0 : 1,
+                  filter:
+                    hoveredSkill && hoveredSkill !== skill.name
+                      ? "brightness(0.5)"
+                      : "brightness(1)",
+                  cursor: "pointer",
+                }}
+              >
+                <FontAwesomeIcon icon={skill.icon} />
+              </div>
+            </div>
+            <div
+              key={index}
+              className={styles.skillCategory}
+              style={{
+                display:
+                  selectedSkill && selectedSkill !== skill.name
+                    ? "none"
+                    : "block",
+                opacity: hoveredSkill && hoveredSkill !== skill.name ? 0.3 : 1,
+                filter:
+                  hoveredSkill && hoveredSkill !== skill.name
+                    ? "brightness(0.5)"
+                    : "brightness(1)",
+              }}
+            >
+              <h3>{skill.name}</h3>
+              <p>{skill.description}</p>
+            </div>
           </div>
         ))}
       </article>
